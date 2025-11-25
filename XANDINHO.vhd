7857a04be57b1081c80ef87fcbd230a0f03b0d35
Julia05dev <julianeves.ufsj@aluno.ufsj.edu.br>
2025-11-25 14:55:43 -0300

    the creation of xandinho

---
+++ XANDINHO.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Xandão e Xandona
+-- 
+-- Create Date:    14:38:16 11/25/2025 
+-- Design Name: 
+-- Module Name:    xandinho - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xandinho is
+port(
+	clk, inicializar : in std_logic;
+	debugEndereco : in std_logic_vector (9 downto 2);
+	debugPalavra : out std_logic (9 downto 2)
+);
+end xandinho;
+
+architecture Behavioral of xandinho is
+--pc
+component 
+--memInstrucoes
+--unidade_controle
+--mux2
+--banco_registradores
+--extensor_sinal
+--uc_ula
+--ula_32
+--memDados
+--somador
+--deslocador_2
+
+begin
+
+
+end Behavioral;
+
