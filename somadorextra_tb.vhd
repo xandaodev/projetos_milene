8927f92e955b3420f70468ca96e6833addafde33
Alexandre Vital <160866761+xandaodev@users.noreply.github.com>
2025-12-04 14:15:44 -0300

    Update somadorextra_tb.vhd

--- somadorextra_tb.vhd
+++ somadorextra_tb.vhd
@@ -1,44 +1,11 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   14:10:28 12/04/2025
--- Design Name:   
--- Module Name:   /export/convidado/XANDINHOO/somadorextra_tb.vhd
--- Project Name:  XANDINHOO
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: somador_extra2
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 ENTITY somadorextra_tb IS
 END somadorextra_tb;
- 
-ARCHITECTURE behavior OF somadorextra_tb IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
+
+ARCHITECTURE behavior OF somadorextra_tb IS
+
     COMPONENT somador_extra2
     PORT(
          A : IN  std_logic_vector(31 downto 0);
@@ -46,47 +13,44 @@ ARCHITECTURE behavior OF somadorextra_tb IS
          resultado : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
-    
 
-   --Inputs
+   -- Inputs
    signal A : std_logic_vector(31 downto 0) := (others => '0');
    signal B : std_logic_vector(31 downto 0) := (others => '0');
 
- 	--Outputs
+   -- Output
    signal resultado : std_logic_vector(31 downto 0);
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
- 
-   constant <clock>_period : time := 10 ns;
- 
+
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
+
    uut: somador_extra2 PORT MAP (
           A => A,
           B => B,
           resultado => resultado
         );
 
-   -- Clock process definitions
-   <clock>_process :process
-   begin
-		<clock> <= '0';
-		wait for <clock>_period/2;
-		<clock> <= '1';
-		wait for <clock>_period/2;
-   end process;
- 
-
-   -- Stimulus process
    stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
+   begin
 
-      wait for <clock>_period*10;
+      -- Teste 1
+      A <= x"00000005";
+      B <= x"00000003";
+      wait for 20 ns;
 
-      -- insert stimulus here 
+      -- Teste 2
+      A <= x"FFFFFFFF";
+      B <= x"00000001";
+      wait for 20 ns;
+
+      -- Teste 3
+      A <= x"0000AAAA";
+      B <= x"00005555";
+      wait for 20 ns;
+
+      -- Teste 4
+      A <= x"12345678";
+      B <= x"11111111";
+      wait for 20 ns;
 
       wait;
    end process;
